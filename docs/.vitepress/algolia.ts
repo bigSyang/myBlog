6d1266b99fb832f4bc9713348ef35b13a8d560d2
杨思阳 <276921372@qq.com>
2023-03-10 14:44:52 +0800

    mod docsearch配置修改

--- docs/.vitepress/algolia.ts
+++ docs/.vitepress/algolia.ts
@@ -1,7 +1,7 @@
 export default {
     appId: 'Y7RQ1RDEX0',
     apiKey: 'f5e32e92aa16f80db6f92a35c792f89a',
-    indexName: 'blogdocs', // index 索引名字
+    indexName: 'helpdocs', // index 索引名字
     placeholder: '请输入关键词',
     translations: {
       button: {
